31e81bc12d7a82b7e241e3fafc7c36afd37c2995
manuredd <74529341+manuredd@users.noreply.github.com>
2022-08-10 11:18:13 -0600

    icon working window size

--- newprojectgui.py
+++ newprojectgui.py
@@ -1,22 +1,25 @@
+import sys
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow
-import sys
+from PyQt6.QtGui import QIcon
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("New Project")
-        self.setWindowIcon("icon.jpeg")
+        self.setFixedSize(QSize(700, 600))
+        app.setWindowIcon(QIcon("icon.jpeg"))
 
 
 app = QApplication(sys.argv)
 
-window = QMainWindow()
+window = MainWindow()
 window.show()
 
 
 if __name__ == "__main__":
+    app.setWindowIcon(QIcon("icon.png"))
     app.exec()
 
 
