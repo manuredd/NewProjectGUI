c98338e86831e1777ae6ea354bde307b096ecffe
manuredd <74529341+manuredd@users.noreply.github.com>
2022-08-18 12:07:26 -0500

    token working

--- gitapi.py
+++ gitapi.py
@@ -1,5 +1,6 @@
 import requests
 from pprint import pprint
+import subprocess
 
 txt = open("/Users/mredd/Misc/token.txt", "r")
 url = "https://api.github.com/user/repos"
@@ -9,7 +10,7 @@ headers = {
     "Accept": "application/vnd.github+json",
     "Authorization": "token " + token,
 }
-if __name__ == "__main__":
-    r = requests.post(url, data=payload, headers=headers)
-
-pprint(r.json())
+#if __name__ == "__main__":
+#    r = requests.post(url, data=payload, headers=headers)
+subprocess.run("cd /Users/mredd")
+#pprint(r.json())
